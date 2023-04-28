4cb383ee7eda0c9eb3d04e324608ad236624dc05
Frieder Singer <friedersinger@users.noreply.github.com>
2023-04-28 10:04:24 +0200

    classes

---
+++ js/game.js
@@ -0,0 +1,10 @@
+let canvas;
+let ctx;
+let character = new Character();
+
+function init() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+
+  console.log("My Character is", character);
+}
