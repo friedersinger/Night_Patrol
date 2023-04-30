ddae48f246c26bb555a82a5a2a224cd37988fe4d
Frieder Singer <friedersinger@users.noreply.github.com>
2023-04-30 22:22:21 +0200

    bug fixes 3

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -33,11 +33,11 @@ class StatusBar extends DrawableObject {
       return 5;
     } else if (this.percentage > 80) {
       return 4;
-    } else if (this.percentage > 80) {
+    } else if (this.percentage > 60) {
       return 3;
-    } else if (this.percentage > 80) {
+    } else if (this.percentage > 40) {
       return 2;
-    } else if (this.percentage > 80) {
+    } else if (this.percentage > 20) {
       return 1;
     } else {
       return 0;
