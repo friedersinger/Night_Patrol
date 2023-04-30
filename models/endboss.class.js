5e5ee525a4ccbe4580e3acff5a20b98b1605a8bc
Frieder Singer <friedersinger@users.noreply.github.com>
2023-04-30 13:20:31 +0200

    endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -15,7 +15,7 @@ class Endboss extends MovableObject {
   constructor() {
     super().loadImage(this.IMAGES_WALKING[0]);
     this.loadImages(this.IMAGES_WALKING);
-    this.x = 500;
+    this.x = 2000;
     this.animate();
   }
 
